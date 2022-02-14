f366b5f456c67abdcc618aa250230e35f76d3229
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-02-14 15:31:44 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TimeToPillApp());
+}
+
+class TimeToPillApp extends StatelessWidget {
+  const TimeToPillApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
